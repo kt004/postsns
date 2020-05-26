b9371ab4201f488bb7b6c03e0898d78fc9a255ef
ktstjh <ktst.jh@gmail.com>
2020-05-26 23:42:35 +0900

    commit 1

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,77 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user, {only: [:index, :show, :edit, :update]}
+  before_action :forbid_login_user, {only: [:new, :create, :login_form, :login]}
+  before_action :ensure_correct_user, {only: [:edit, :update]}
+  def ensure_correct_user
+  	  if @current_user.id != params[:id].to_i
+  	  	  flash[:notice] = "no authority"
+  	  	  redirect_to("/posts/index")
+  	  end
+  end
+  def index
+  	  @users = User.all.order(created_at: :desc)
+  end
+  def show
+  	  @user = User.find_by(id: params[:id])
+  end
+  def new
+  	  @user = User.new
+  end
+  def create
+  	  @user = User.new(
+  	  	name: params[:name], 
+  	  	email: params[:email], 
+  	  	image_name: "default_user.jpg",
+  	  	password: params[:password])
+  	  if @user.save
+  	  	  session[:user_id] = @user.id
+  	  	  flash[:notice] = "user registration completed"
+  	  	  redirect_to("/users/#{@user.id}")
+  	  else
+  	  	  render("users/new")
+  	  end
+  end
+  def edit
+  	  @user = User.find_by(id: params[:id])
+  end
+  def update
+  	  @user = User.find_by(id: params[:id])
+  	  @user.name = params[:name]
+  	  @user.email = params[:email]
+  	  if params[:image]
+  	  	  @user.image_name = "#{@user.id}.jpg"
+  	  	  image = params[:image]
+  	  	  File.binwrite("public/user_images/#{@user.image_name}", image.read)  	  
+  	  end
+  	  if @user.save
+  	  	  flash[:notice] = "user information edited"
+  	  	  redirect_to("/users/#{@user.id}")
+  	  else
+  	  	  render("/users/edit")
+  	  end
+  end
+  def login_form
+  end
+  def login
+  	  @user = User.find_by(	email: params[:email])
+  	  if @user && @user.authenticate(params[:password])
+  	  	  session[:user_id] = @user.id
+  	  	  flash[:notice] = "logined"
+  	  	  redirect_to("/posts/index")
+  	  else
+  	  	  @error_message = "wrong email address or password"
+  	  	  @email = params[:email]
+  	  	  @password = params[:password]
+  	  	  render("users/login_form")
+  	  end
+  end
+  def logout
+  	  session[:user_id] = nil
+  	  flash[:notice] = "logouted"
+  	  redirect_to("/login")
+  end
+  def likes
+  	  @user = User.find_by(id: params[:id])
+  	  @likes = Like.where(user_id: @user.id)
+  end
+end
